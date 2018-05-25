bc37067043ce4f7b9b5aea364bb71b9c664c7695
aegan123 <juhani@fmail.co.uk>
2018-05-25 17:07:35 +0300

    Combined all visualizing into one place

--- MyUI.java
+++ MyUI.java
@@ -1,7 +1,5 @@
 package com.example.villeprojekti;
 
-import java.io.File;
-
 import javax.servlet.annotation.WebServlet;
 
 import org.vaadin.hezamu.canvas.Canvas;
@@ -9,13 +7,10 @@ import org.vaadin.hezamu.canvas.Canvas.CanvasImageLoadListener;
 
 import com.vaadin.annotations.Theme;
 import com.vaadin.annotations.VaadinServletConfiguration;
-import com.vaadin.server.FileResource;
 import com.vaadin.server.VaadinRequest;
-import com.vaadin.server.VaadinService;
 import com.vaadin.server.VaadinServlet;
 import com.vaadin.ui.Button;
 import com.vaadin.ui.HorizontalLayout;
-import com.vaadin.ui.Image;
 import com.vaadin.ui.Label;
 import com.vaadin.ui.TextField;
 import com.vaadin.ui.UI;
@@ -32,14 +27,16 @@ import com.vaadin.ui.VerticalLayout;
  */
 @Theme("mytheme")
 public class MyUI extends UI {
+	/** "html5" canvas to draw images to.  */
 	private Canvas canvas;
+	/** The generated assignment. */
+	private FruitstackGenerator stack;
 	private JoulukuusiGenerator kuusi;
-	private String baseurl="https://liquid-moon.pw/utu/";
-	private  String basepath = VaadinService.getCurrent().getBaseDirectory().getAbsolutePath();
-	private TextField text1=new TextField();
-	private TextField text2=new TextField();
-	private TextField text3=new TextField();
-	private TextField text4=new TextField();
+	/**Base url to load pictures from. Actual filename will be appended to it later. */
+	private final static String baseurl="https://liquid-moon.pw/utu/";
+	private VerticalLayout mainContent,fruitcontent,content;
+	private HorizontalLayout kuusiContent;
+	private TextField text1,text2,text3,text4;
 	
 
     /**Foobar yolo! :D	 */
@@ -47,103 +44,120 @@ public class MyUI extends UI {
 
 	@Override
     protected void init(VaadinRequest vaadinRequest) {
-		initKuusi();
-		//initFruit();
-
-		/*
-        content.setExpandRatio(layout1, 1);
-        final TextField name = new TextField();
-        name.setCaption("Type your name here:");
-
-        Button button = new Button("Click Me");
-        button.addClickListener( e -> {
-            layout.addComponent(new Label("Thanks " + name.getValue() 
-                    + ", it works!"));
-        });
-        
-        //layout.addComponents(name, button);
-        JoulukuusiGenerator kuusi=new JoulukuusiGenerator();
-        Label label=new Label(kuusi.getBinarymatrix().toString());
-        TextField text=new TextField();
-        text.setCaption("Your answer:");
-        Button button=new Button("Check answer.");
-        button.addClickListener( e -> {
-            layout.addComponent(new Label(Boolean.toString(kuusi.isRightAnswer(text.getValue()))));
-        });
-        layout.addComponents(label,text,button);
-        layout.setMargin(true);
-        layout.setSpacing(true);
-        
-        
-        
-
-        //foobar(layout,kuusi);
-        //Image image2 = new Image("", resource2);
-
-        //layout.addComponents(image,image2);
-*/
+	mainContent=new VerticalLayout();
+	HorizontalLayout buttons=new HorizontalLayout();
+	Button joulukuusi=new Button("Joulukuusi");
+	Button fruit=new Button("Hedelmäpino");
+	Button empty=new Button("Tyhjennä");
+	empty.addClickListener( e -> {
+	       reset();
+	     });
+	joulukuusi.addClickListener( e -> {
+       initKuusi();
+     });
+	fruit.addClickListener( e -> {
+        initFruit();
+     });
+	buttons.addComponents(joulukuusi,fruit,empty);
+	content=new VerticalLayout();
+	mainContent.addComponents(buttons,content);
+	setContent(mainContent);
     }
+	/**
+	 * Resets components in main view.
+	 * For demo purposes only.
+	 */
+	private void reset() {
+		try {
+			content.removeAllComponents();
+			canvas.clear();
+			}
+			catch(NullPointerException e) {
+				
+			}
+	}
+/**
+ * Initializes fruit stack ui.
+ * For demo purposes only.
+ */
 	private void initFruit() {
+		reset();
+		stack=new FruitstackGenerator(true,4,false);
+		if(stack.getTypeOfTask())	initMultiFruit();
+		else initDIYFruit();
+	}
+	private void initDIYFruit() {
 		// TODO Auto-generated method stub
 		
 	}
-	private void initKuusi() {
-		kuusi=new JoulukuusiGenerator();
-		VerticalLayout content=new VerticalLayout();
-		/*
-		String[]urls={basepath + "/WEB-INF/images/light_on.svg",basepath + "/WEB-INF/images/light_off.svg",basepath + "/WEB-INF/images/christmas_tree.svg"};
-		//String[]urls={"localhost:8081/WEB-INF/images/light_on.svg","localhost:8081/WEB-INF/images/light_off.svg","localhost:8081/WEB-INF/images/christmas_tree.svg"};
-		setContent(content);
-        HorizontalLayout layout1 = new HorizontalLayout();
-        HorizontalLayout layout2 = new HorizontalLayout();
-        HorizontalLayout layout3 = new HorizontalLayout();
-        HorizontalLayout layout4 = new HorizontalLayout();
-        HorizontalLayout layout5 = new HorizontalLayout();
-        layout1.setSizeFull();
-        layout2.setSizeFull();
-        layout3.setSizeFull();
-        layout4.setSizeFull();
-        //Table table=new Table("Foobar");
-        //table.addItem(new Label[] {new Label("0"),new Label("0000")}, null);
-        //table.setSizeFull();
-        //content.addComponents(new Label("0: 0000"),new Label("1: 0001"),new Label("2: 0010"),new Label("3: 0011"),new Label("4: 0100"),new Label("5: 0101"),new Label("6: 0110"),new Label("7: 0111"),new Label("8: 1000"),new Label("9: 1001"));
-        //content.addComponent(table);
-        layout5.setCaption("<table><tr><th>Decimal</th><th>Binary</th></tr><tr><td>0</td><td>0000</td></tr></table>");
-        layout5.setCaptionAsHtml(true);
-        content.addComponent(layout5);
-        //content.addComponent(new Label(basepath));
-       addImg(layout1,0,kuusi);
-        layout1.addComponent(text1);
-        content.addComponent(layout1);
-        addImg(layout2,1, kuusi);
-        layout2.addComponent(text2);
-        content.addComponent(layout2);
-        addImg(layout3,2, kuusi);
-        layout3.addComponent(text3);
-        content.addComponent(layout3);
-        addImg(layout4,3, kuusi);
-        layout4.addComponent(text4);
-        content.addComponent(layout4);
-        
-        Button button=new Button("Check answer.");
-        button.addClickListener( e -> {
-            content.addComponent(new Label(Boolean.toString(kuusi.isRightAnswer(text1.getValue()+text2.getValue()+text3.getValue()+text4.getValue()))));
-        });
-        content.addComponent(button);
-        */
-        
+
+	private void initMultiFruit() {
+		fruitcontent=new VerticalLayout();
 		//content.addComponent(canvas = new Canvas());
 		canvas=new Canvas();
-		canvas.setHeight(64*4+10, Unit.PIXELS);
+		canvas.setHeight(50*stack.getSize()+10, Unit.PIXELS);
+		canvas.setWidth(500, Unit.PIXELS);
 		HorizontalLayout hlayout=new HorizontalLayout();
-
-        //canvas.fillRect(10, 10, 200, 200);
-        //String[] urls=genUrls(kuusi);
-        //String[]urls= {basepath+"/WEB-INF/images/light_off.svg",basepath+"/WEB-INF/images/light_on.svg"};
+        String[]urls= {baseurl+"banana.svg",baseurl+"apple.svg",baseurl+"lemon.svg"};
+		canvas.loadImages(urls);
+		canvas.addImageLoadListener(new CanvasImageLoadListener() {
+			
+			@Override
+			public void imagesLoaded() {
+				drawImg();
+				}
+		});
+		Button button1=new Button("1");
+		button1.addClickListener( e -> {
+           fruitcontent.addComponent(new Label("1: "+Boolean.toString(stack.isRightAnswer(0))));
+        });
+		Button button2=new Button("2");
+		button2.addClickListener( e -> {
+           fruitcontent.addComponent(new Label("2: "+Boolean.toString(stack.isRightAnswer(1))));
+        });
+		Button button3=new Button("3");
+		button3.addClickListener( e -> {
+           fruitcontent.addComponent(new Label("3: "+Boolean.toString(stack.isRightAnswer(2))));
+        });
+		Button button4=new Button("4");
+		button4.addClickListener( e -> {
+           fruitcontent.addComponent(new Label("4: "+Boolean.toString(stack.isRightAnswer(3))));
+        });
+		hlayout.addComponents(button1,button2,button3,button4);
+		fruitcontent.addComponents(new Label(stack.getCommand()),canvas,hlayout);
+		//setContent(fruitcontent);
+		content.addComponent(fruitcontent);
 		
-		//Picture source: https://commons.wikimedia.org/w/index.php?title=File:Simpleicons_Interface_light-bulb-outline.svg&oldid=189680700
-		//Licensed under CC BY 3.0 (https://creativecommons.org/licenses/by/3.0/deed.en)
-        String[]urls= {baseurl+"light_off.svg",baseurl+"light_on.svg"};
+	}
+	/**
+	 * Actually draws the images to canvas after they have been preloaded by the browser.
+	 * Called by CanvasImageLoadListener.
+	 */
+	private void drawImg() {
+		//kuva on 400x400px
+		double x=0.0, y=0.0;
+		for(int i=0;i<4;i++) {
+			for(int j=stack.getSize();j>0;j--) {
+			canvas.drawImage2(baseurl+stack.getPic(i, j-1), x, y,50.0,50.0);
+			y+=50;
+			}
+			y=0;
+			x+=50;
+		}
+		
+	}
+	/**
+	 * Initaliazes "christmas tree" ui.
+	 * For demo purposes only.
+	 */
+	private void initKuusi() {
+		reset();
+		kuusi=new JoulukuusiGenerator();
+		kuusiContent=new HorizontalLayout();
+		canvas=new Canvas();
+		canvas.setHeight(64*4+10, Unit.PIXELS);
+		
+		String[]urls= {baseurl+"light_off.svg",baseurl+"light_on.svg"};
 		canvas.loadImages(urls);
 		canvas.addImageLoadListener(new CanvasImageLoadListener() {
 			
@@ -165,49 +179,22 @@ public class MyUI extends UI {
 					x=0.0;
 					y+=65;
 				}
-				//canvas.drawImage1(urls[0], 0.0, 0.0);
-				//canvas.drawImage1(urls[1], 65.0, 0.0);
 				}
-
-			
 		});
 		VerticalLayout layout=new VerticalLayout();
 		  Button button=new Button("Check answer.");
 	        button.addClickListener( e -> {
 	            layout.addComponent(new Label(Boolean.toString(kuusi.isRightAnswer(text1.getValue()+text2.getValue()+text3.getValue()+text4.getValue()))));
 	        });
+	    	text1=new TextField();
+	    	text2=new TextField();
+	    	text3=new TextField();
+	    	text4=new TextField();
 		layout.addComponents(text1,text2,text3,text4,button);
-		hlayout.addComponents(canvas,layout);
-		//content.addComponents(hlayout,layout);
-		setContent(hlayout);
-		
+		kuusiContent.addComponents(canvas,layout);
+		content.addComponent(kuusiContent);
 	}
-	private void addImg(HorizontalLayout layout, int i, JoulukuusiGenerator kuusi) {
-        FileResource resource=null;
-        Image image=null;
-        	for(int j=0;j<4;j++) {
-        			if(kuusi.isOn(i, j)) {
-        					resource = new FileResource(new File(basepath + "/WEB-INF/images/light_on.svg"));
-        			}
-        			else {
-        					resource = new FileResource(new File(basepath + "/WEB-INF/images/light_off.svg"));
-        			}
-         image = new Image("", resource);
-         image.setHeight(50, Unit.PERCENTAGE);
-         image.setWidth(50, Unit.PERCENTAGE);
-         layout.addComponent(image);
-        }
-	}
-    /*   private String[] genUrls(JoulukuusiGenerator kuusi) {
-		ArrayList<String> temp=new ArrayList<String>();
-		for(int i=0;i<4;i++) {
-			for(int j=0;j<4;j++) {
-				temp.add(kuusi.getLamppu(i, j).toString().substring(2));
-			}
-		}
-		return temp.toArray(new String[temp.size()]);
-		}
-*/
+
 	@WebServlet(urlPatterns = "/*", name = "MyUIServlet", asyncSupported = true)
     @VaadinServletConfiguration(ui = MyUI.class, productionMode = false)
     public static class MyUIServlet extends VaadinServlet {
